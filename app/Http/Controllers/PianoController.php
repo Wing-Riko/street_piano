b55a927dad1a560a0a42a4c9065435a31003c868
Wing-Riko <ricozmicky@gmail.com>
2023-01-04 22:30:07 +0900

    pianos表示

---
+++ app/Http/Controllers/PianoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Piano;
+
+class PianoController extends Controller
+{
+    public function index(Piano $piano)
+    {
+        return view('pianos/index')->with(['pianos' => $piano->get()]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PianoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    echo date_default_timezone_get();
     return view('welcome');
 });
+
+Route::get('/pianos', [PianoController::class, 'index']);
